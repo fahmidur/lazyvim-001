d50da05b2a32a271ff5286a3cb4acc0abd8980d6
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-12-10 22:25:37 -0800

    more LSPs

--- init.lua
+++ init.lua
@@ -3,8 +3,11 @@ require("config.lazy")
 vim.keymap.set('n', ';', ':')
 
 lspconfig = require('lspconfig')
-lspconfig.clangd.setup{} -- LS for C/C++
+lspconfig.clangd.setup({})   -- LS for C/C++
+lspconfig.tsserver.setup({}) -- LS for TypeScript
+lspconfig.gopls.setup({})    -- LS for Golang
 
 symbolsoutline = require("symbols-outline")
 symbolsoutline.setup()
 vim.keymap.set('n', ',r', '<Cmd>SymbolsOutline<CR>')
+
