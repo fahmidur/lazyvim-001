681fe1f8b48b35068348927a282dc3f9a11e79b9
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-12-10 22:01:26 -0800

    symbols-outline

--- init.lua
+++ init.lua
@@ -2,5 +2,9 @@
 require("config.lazy")
 vim.keymap.set('n', ';', ':')
 
-require("symbols-outline").setup()
+lspconfig = require('lspconfig')
+lspconfig.clangd.setup{} -- LS for C/C++
 
+symbolsoutline = require("symbols-outline")
+symbolsoutline.setup()
+vim.keymap.set('n', ',r', '<Cmd>SymbolsOutline<CR>')
